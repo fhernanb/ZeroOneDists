1acc54351f9272744643e8d53e5d3d115c4606d9
fhernanb <fhernanb@unal.edu.co>
2025-05-18 17:50:05 -0500

    cambio menor en dUMB

--- R/dUMB.R
+++ R/dUMB.R
@@ -31,67 +31,65 @@
 #' @example examples/examples_dUMB.R
 #'
 #' @export
-#' @importFrom stats dbeta
 dUMB <- function(x, mu = 1, log = FALSE) {
   if (any(mu <= 0)) stop("parameter mu must be positive!")
 
-  sapply(x, function(xi) {
-    if (xi <= 0 || xi >= 1) return(if (log) -Inf else 0)
+  log_term <- log(1 / x)
+  res <- 0.5 * log(2 / pi) + 2 * log(log_term) - 3 * log(mu) - log(x) - (log_term^2) / (2 * mu^2)
 
-    log_term <- log(1 / xi)
-    log_density <- 0.5 * log(2 / pi) + 2 * log(log_term) - 3 * log(mu) - log(xi) - (log_term^2) / (2 * mu^2)
-
-    if (log) return(log_density)
-    else return(exp(log_density))
-  })
+  if(log)
+    return(res)
+  else
+    return(exp(res))
 }
 #' @export
-#' @importFrom stats pnorm
 #' @rdname dUMB
 pUMB <- function(q, mu = 1, lower.tail = TRUE, log.p = FALSE) {
-  if (any(mu <= 0)) stop("parameter mu must be positive!")
-
   erf <- function(x) 2 * pnorm(x * sqrt(2)) - 1
 
-  sapply(q, function(qi) {
-    if (qi <= 0) {
-      return(if (log.p) -Inf else 0)
-    }
-    if (qi >= 1) {
-      return(if (log.p) 0 else 1)
-    }
-
-    log_term <- log(1 / qi)
+  # Begin aux fun
+  auxfun <- function(q) {
+    log_term <- log(1 / q)
     erf_arg <- log_term / (sqrt(2) * mu)
     erf_part <- erf(erf_arg)
-
     term1 <- (sqrt(log_term^2) * erf_part) / log_term
     term2 <- (sqrt(2 / pi) * log_term * exp(-log_term^2 / (2 * mu^2))) / mu
+    res <- 1 - term1 + term2
+    res
+  }
+  # End aux fun
 
-    cdf <- 1 - term1 + term2
-    if (!lower.tail) cdf <- 1 - cdf
-    if (log.p) cdf <- log(cdf)
-    return(cdf)
-  })
+  cdf <- ifelse(q<=0, 0,
+                ifelse(q >=1, 1, auxfun(q)))
+
+  if (!lower.tail) cdf <- 1 - cdf
+  if (log.p) cdf <- log(cdf)
+  return(cdf)
 }
 #' @export
 #' @importFrom stats uniroot
 #' @rdname dUMB
-qUMB <- function(p, mu = 1) {
-  if (any(mu <= 0)) stop("parameter mu must be positive!")
-  if (any(p < 0 | p > 1)) stop("p must be in [0, 1]")
+qUMB <- function(p, mu, lower.tail = TRUE, log.p = FALSE) {
+  if (any(p < 0 | p > 1)) stop("p must be in the interval (0, 1)")
+  if (any(mu < 0))        stop("mu must be positive")
 
-  quantile_fn <- function(prob) {
-    sapply(prob, function(pp) {
-      if (pp == 0) return(0)
-      if (pp == 1) return(1)
-      uniroot(function(y) pUMB(y, mu = mu) - pp,
-              lower = .Machine$double.eps, upper = 1 - .Machine$double.eps,
-              tol = 1e-9)$root
-    })
+  if (log.p == TRUE)
+    p <- exp(p)
+  else p <- p
+  if (lower.tail == TRUE)
+    p <- p
+  else p <- 1 - p
+
+  aux_fun <- function(x, p, mu){
+    res <- p - pUMB(x, mu)
+    return(res)
   }
-  return(quantile_fn(p))
+  res <- uniroot(aux_fun, interval=c(0, 1),
+                 p=p, mu=mu)$root
+
+  return(res)
 }
+qUMB <- Vectorize(qUMB)
 #' @export
 #' @importFrom stats runif
 #' @rdname dUMB
